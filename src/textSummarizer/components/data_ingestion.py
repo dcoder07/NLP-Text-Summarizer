0e2bb8d28d0632313429c8df697bb9042421d278
Dishank Patil <dishankjp05@gmail.com>
2025-08-24 00:41:53 +0530

    data validation completed

--- src/textSummarizer/components/data_ingestion.py
+++ src/textSummarizer/components/data_ingestion.py
@@ -33,5 +33,4 @@ class DataIngestion:
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
-            
+            zip_ref.extractall(unzip_path)     
